8a05b200f81ddb15e895b3977945d114ee706803
Dan Thompson <info@danthompsonguitar.co.uk>
2025-06-02 22:45:24 +0100

    refactor: remove unused MongoDB client and collections from main server file

--- server/main.py
+++ server/main.py
@@ -1,18 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient
 
 from src.routes.objectrouter import router as object_router  # Import the object router
 from src.routes.schemarouter import router as schema_router
 
 app = FastAPI()
-
-client = AsyncIOMotorClient('mongodb://mongodb:27017')
-db = client['reservation-system']
-collection = db['schemas']
-objects_collection = db['objects']
-topologies_collection = db['topologies']  # New collection for topologies
-reservations_collection = db['reservations']  # New collection for reservations
 
 # Include the routers
 app.include_router(schema_router, prefix="/schemas", tags=["schemas"])
